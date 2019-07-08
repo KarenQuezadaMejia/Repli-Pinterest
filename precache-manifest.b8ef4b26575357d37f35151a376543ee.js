5bb9b5dbf35812244441e1e83ba245a685fa854d
KarenQuezadaMejia <quezadakaren16@gmail.con>
2019-07-08 14:50:59 -0400

    Updates

---
+++ precache-manifest.b8ef4b26575357d37f35151a376543ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de023f79019263c814ae796a894f4fe",
+    "url": "/pinterest/index.html"
+  },
+  {
+    "revision": "a095d8e0c633dc5e9721",
+    "url": "/pinterest/static/css/main.8b14f194.chunk.css"
+  },
+  {
+    "revision": "2310902dea5dc0625e05",
+    "url": "/pinterest/static/js/2.b9f93adb.chunk.js"
+  },
+  {
+    "revision": "a095d8e0c633dc5e9721",
+    "url": "/pinterest/static/js/main.c1caff00.chunk.js"
+  },
+  {
+    "revision": "9cbd3f092346cd350b1c",
+    "url": "/pinterest/static/js/runtime~main.9d05082c.js"
+  }
+]);
